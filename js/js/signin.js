d5897306fa157e46f4e2eb52d75afc0b98be64c0
Michael Pincus <mikepincus1@aim.com>
2014-03-31 17:36:46 -0400

    signup update / regex update

--- js/js/signin.js
+++ js/js/signin.js
@@ -1,9 +1,24 @@
-function signupMsg(){
-	localStorage.setItem("username", document.getElementById('userid').value);
-	alert('Welcome ' + localStorage.getItem("username"));
+function signupMsg() {
+    passCheck();
+    if (passCheck()) {
+        localStorage.setItem("username", document.getElementById('userid').value);
+        alert('Welcome ' + localStorage.getItem("username"));
+    }
 }
-function loggedInMsg(){
-    if(localStorage.getItem('userid') != undefined){
-	    var str = '<p>Welcome Back ' + localStorage.getItem('username')+'</p>';
-	    document.getElementById('userMsg').innerHTML=str;
-}   }
+function loggedInMsg() {
+    if (localStorage.getItem('userid') != undefined) {
+        var str = '<p>Welcome Back ' + localStorage.getItem('username') + '</p>';
+        document.getElementById('userMsg').innerHTML = str;
+    }
+}
+function passCheck() {
+    if (docuent.getElementById('cpswrd')) {
+        if (docuement.getElementById('pswrd').value == document.getElementById('cpswrd').value) {
+            return true;
+        }
+        else
+            return false;
+    }
+    else
+        return true;
+}
