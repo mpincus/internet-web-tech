7d6cd6bb25ac45205ad051580daa967aedbdd8f4
mpincus <mikepincus1@aim.com>
2014-04-16 23:49:48 -0400

    ajax complete

--- ajax/js/nutrition.js
+++ ajax/js/nutrition.js
@@ -2,37 +2,26 @@ var asyncRequest;
 
 function getInfo() {
     try {
-        alert("stuff1");
         asyncRequest = new XMLHttpRequest();
-        asyncRequest.addEventListener("readystatechange", function () {
-            processResponse();
-        }, false);
-        asyncRequest.open("GET", "xml/nutrition.xml", true);
-        asyncRequest.send(null);
+        asyncRequest.open("GET", "nutrition.xml", false);
+        asyncRequest.send();
 
-    }
-    catch (exception) {
-        alert("request failed");
-
-    }
-    return false;
-}
-function processResponse() {
-
-    // if (asyncRequest.readyState == 4 && asyncRequest.status == 200 && asyncRequest.responseXML) {
-    alert("stuff1");
-    var dishes = asyncRequest.responseXML.getElementsByTagName("dish");
-    var dishName = document.getElementById('dish').value;
-    for (var i = 0; i < dishes.length; i++) {
-        var dish = dishes.item(i);
-        var name = dish.getElementsByTagName('name').item(0).firstChild.nodeValue;
-        if (name == dishName) {
-            document.getElementById('contentArea').innerHTML = dish.getElementById('name').item(0).firstChild.nodeValue;
-            document.getElementById('contentArea').innerHTML = dish.getElementById('a').item(0).firstChild.nodeValue;
-            document.getElementById('contentArea').innerHTML = dish.getElementById('b').item(0).firstChild.nodeValue;
-            document.getElementById('contentArea').innerHTML = dish.getElementById('c').item(0).firstChild.nodeValue;
-            document.getElementById('contentArea').innerHTML = dish.getElementById('d').item(0).firstChild.nodeValue;
+        var dishes = asyncRequest.responseXML.getElementsByTagName("dish");
+        var dishName = document.getElementById('dish').value;
+        for (var i = 0; i < dishes.length; i++) {
+            var name = dishes[i].getElementsByTagName('name')[0].firstChild.nodeValue;
+            if (name == dishName) {
+                document.getElementById('name').innerHTML = "<p>"+ dishes[i].getElementsByTagName('name')[0].firstChild.nodeValue + "</p>";
+                document.getElementById('a').innerHTML =  "<p>"+ dishes[i].getElementsByTagName('a')[0].firstChild.nodeValue + "</p>";
+                document.getElementById('b').innerHTML =  "<p>"+ dishes[i].getElementsByTagName('b')[0].firstChild.nodeValue + "</p>";
+                document.getElementById('c').innerHTML = "<p>"+ dishes[i].getElementsByTagName('c')[0].firstChild.nodeValue + "</p>";
+                document.getElementById('d').innerHTML = "<p>"+ dishes[i].getElementsByTagName('d')[0].firstChild.nodeValue + "</p>";
+            }
         }
     }
-    // }
+    catch (exception) {
+        alert(exception);
+    }
+
+    return false;
 }
